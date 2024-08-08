ed1033a16f2cffaafef5679dca9e663e662c309b
Vitorluca <vitor8391244603@gmail.com>
2024-08-07 22:35:28 -0300

    add file main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+#project O salão dos barbeiros dorminhocos
+import threading #biblioteca para criar threads
+import time #biblioteca para controlar o tempo
+import random #biblioteca para gerar números aleatórios
+
